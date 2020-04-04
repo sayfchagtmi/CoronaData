aa94e2af8692a2b7a16e6eef832cd4068c0da6b3
sayfchagtmi <32186698+sayfchagtmi@users.noreply.github.com>
2020-04-04 20:06:34 +0100

    Update save.py

--- corona/save.py
+++ corona/save.py
@@ -6,11 +6,10 @@ def save_data(parsed_page, path=''):
     table = parsed_page.find("table",{"id":"main_table_countries_today"})
     rows = [row for row in table.findAll("tr")]
     headers = [th.text.replace('\n',' ').replace(',',';') for th in rows[0].findAll("th")]
-    if len(path)!=0:
-        if path[-1]=='/':
-            fpath = path+DEFAULT_FILE_NAME
-        else:
-            fpath = path+'/'+DEFAULT_FILE_NAME
+    if path[-1]=='/':
+        fpath = path+DEFAULT_FILE_NAME
+    else:
+        fpath = path+'/'+DEFAULT_FILE_NAME
     f = open(fpath, "w")
     f.write(','.join(headers) + '\n')
     rows = rows[1:]
